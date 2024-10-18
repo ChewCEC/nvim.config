44d0eeafe5498716ed6aa43dc43a9d0870f79312
ChewCEC <camilo.echeverri.100@gmail.com>
2024-10-18 17:26:52 -0500

    Fixed some remaps

--- lua/chewcec/lazy/harpoon.lua
+++ lua/chewcec/lazy/harpoon.lua
@@ -5,7 +5,7 @@ return {
 
     config = function ()
         local harpoon = require("harpoon")
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
@@ -14,8 +14,8 @@ return {
         vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-N>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-M>", function() harpoon:list():next() end)
 
     end
 }
