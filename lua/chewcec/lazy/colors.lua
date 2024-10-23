b1b4025b8db9ffacd61894abe952460d1e9e6b3a
ChewCEC <camilo.echeverri.100@gmail.com>
2024-10-23 01:54:36 -0500

    add theme for status bar

--- lua/chewcec/lazy/colors.lua
+++ lua/chewcec/lazy/colors.lua
@@ -14,10 +14,10 @@ return {
         name = 'rose-pine',
         config = function()
             require('rose-pine').setup({
-                variant = 'moon', 
+                variant = 'moon',
             })
             vim.cmd('colorscheme rose-pine')
         end,
-    }
+    },
 }
 
--- lua/chewcec/lazy/harpoon.lua
+++ lua/chewcec/lazy/harpoon.lua
@@ -8,7 +8,7 @@ return {
 
             harpoon:setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
             vim.keymap.set("n", "<C-d>", function() harpoon:list():select(1) end)
